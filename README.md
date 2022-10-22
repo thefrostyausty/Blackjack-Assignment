e23fc48ef8321ad40188f0edf09f47e7df7eb18d
Austin Robinson <98559337+thefrostyausty@users.noreply.github.com>
2022-10-22 01:53:52 -0400

    Assignment Complete

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
 # Blackjack-Assignment
 
 ## Description
+A simple blackjack game created with Python that is played in the command line for Freenome Internship Assignment! Hope you enjoy!
 
 ## Installation Instructions
 
+1. Fork and Clone this repository
+2. Open your terminal
+3. Run game by using the command `python3 blackjack.py`
+
+## Questions to Answer:
+
+### What assumptions/choices you made to overcome any lack of clarity you found in the instructions?
+
+*I am a person that likes to ask for help, in all things that involve coding. So I reached out to internet searching and fellow coders with small details like what I should remember is needed for the game to function properly.*
+
+### What you did well on this project
+
+*I think I did well with pseudocoding my thoughts and how things worked out. I also think my code is readable and easy to understand*
+
+
+### Rationale on design choices, algorithmic decisions made
+ 
+*I kept everything simple and algorithmically I think it could be a little cleaner however I still struggle with that concept*
+
+### Tradeoffs you encountered while programming and how you resolved them
+
+*Again, I like to ask around rather than suffer in silence so that helped me best and even my books I've purchased about programming helped me as well.*
+
+### What you would improve on this project given more time
+
+*Adding more dialogue between the game and the player's choices and final results*
+
+### What manual tests you ran on the code
+
+*If by manual tests, meaning running through the game, I've played it about 3 times*
+
+### How to run any automated tests you created (if they exist)
+
+*No automated tests ran*
--- blackjack.py
+++ blackjack.py
@@ -1,27 +1,93 @@
 import random
+player = True
+dealer = True
+#first we need to create the cards
+deck = [
+    2, 3, 4, 5, 6, 7, 8, 9, 10,
+    2, 3, 4, 5, 6, 7, 8, 9, 10,
+    2, 3, 4, 5, 6, 7, 8, 9, 10,
+    2, 3, 4, 5, 6, 7, 8, 9, 10,
+    'J', 'Q', 'K', 'A',
+    'J', 'Q', 'K', 'A',
+    'J', 'Q', 'K', 'A',
+    'J', 'Q', 'K', 'A'
+        ]
+player_hand = []
+dealer_hand = []
 
-# First we want to create/define a Card class, this will be our deck
-# random has been imported as a way to ensure the deck is being randomized
-# everytime
-class Card:
-    def __init__(self, suit, value):
-        self.suit = suit
-        self.value = value
+#next we need to create a function that deals our cards
+def shuffleCards(turn):
+    #this is how we get a random card out of the deck
+    card = random.choice(deck)
+    #this will add it to the hand
+    turn.append(card)
+    #we want to take that card out the deck
+    deck.remove(card)
 
-    # this function is in order to string the suit and value 
-    # returning a statement 
-    def __repr__(self):
-        return "'' of ''".join((self.value, self.suit))
+# we then need to calculate the hands 
+def total(turn):
+    total = 0
+    faceCards = ['J', 'Q', 'K',]
+    for card in turn:
+        if card in range (1, 11):
+            total += card
+        elif  card in faceCards:
+            total += 10
+        else:
+            if total > 11:
+                total += 1
+            else:
+                total +=11
+    return total 
+
+# we now need to check for the winner 
+def showDealerHand():
+    if len(dealer_hand) == 2:
+        return dealer_hand[0]
+    elif len(dealer_hand) > 2:
+        return dealer_hand[0], dealer_hand[1]
 
 
-# Second we need to be able to have a deck
+# now we need to create the game loop
+for game in range(2):
+    shuffleCards(dealer_hand)
+    shuffleCards(player_hand)
+
+while player or dealer:
+    print(f'Dealer had {showDealerHand()} and X')
+    print(f'You have {player_hand} for a total of {total(player_hand)}')
+
+    if player:
+       chooseOption = input("1: Stand or 2: Hit")
+    if total(dealer_hand) > 16:
+         dealer = False
+    else:
+        shuffleCards(dealer_hand)
+    if chooseOption == '1':
+        player = False
+    else:
+        shuffleCards(player_hand)
+    if total(player_hand) >= 21:
+        break
+    elif total(dealer_hand) >= 21:
+        break
 
 
-# Third the deck needs to be dealt
+# now we need to determine the winner
+
+if total(player_hand) == 21:
+    print(f'You have {player_hand} with a score of {total(player_hand)}')
+    print('You win! Blackjack!')
+elif total(dealer_hand) == 21:
+    print(f'The dealer {dealer_hand} has a score of {total(dealer_hand)}')
+    print ('You lost. Deal won Blackjack!')
+elif total(player_hand) > 21:
+    print('You bust! Dealer Wins!')
+elif total(dealer_hand) > 21:
+    print('You won! Dealer bust!')
+elif 21 - total(dealer_hand) < 21 - total(player_hand):
+    print('Dealer Wins!')
+elif 21 - total(dealer_hand) > 21 - total(player_hand):
+    print ('You win!')
 
 
-# Fourth we need to calculate the totals
-
-
-
-# Lastly check for winners   
